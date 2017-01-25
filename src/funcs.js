5da5ea1aac2699c667cfb90f39b71bd385d32153
曹振峰 <purebluesky@yeah.net>
2017-01-25 00:21:46 -0500

    updates

--- src/funcs.js
+++ src/funcs.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   if(isAPIAvailable()) {
-    $('#file').bind('change', handleFileSelect);
+    $('#file').bind('change', prepare);
   };
   $("#cmd").on('keyup', function (e) {
       if (e.keyCode == 13) {
@@ -9,6 +9,39 @@ $(document).ready(function() {
       }
   });
 });
+
+
+function prepare(evt){
+  var file = evt.target.files[0]; // FileList object
+  var reader = new FileReader();
+  reader.readAsText(file);
+  reader.onload = function(event){
+    var csv = event.target.result;
+    window.Vars={};
+    Vars.data=$.csv.toArrays(csv);
+    var tb=Vars.data.slice(0,4);
+    tb.splice(0,0,range(1,tb[0].length,1));
+    printTable(tb);
+    out("You've loaded '"+file.name+"'.");
+    out("Total number of students: "+String(Vars.data.length-1));
+    var reg=/[cC]\d{5,}/;
+    for (i = 0; i < Vars.data[0].length; i++) {
+      x=reg.test(Vars.data[1][i]);
+      if (x){
+        Vars.idf=i;
+        out('The C number is found at column '+(Vars.idf+1)+', and the last 4 digits of it will be used to identify the students.','lime')
+        out('If you want to change the identifier to other column, you can input "idf=xxx", where xxx is the column sequence number','blue');
+        out('If you want to change the number of digits to be used, you can input "nd=xxx", where xxx is the number of digits.','blue');
+        break;
+      };
+      if (i == Vars.data[0].length-1){
+        out('Please set the column for identifying the students by inputting "idf=xxx", where xxx is the sequence number of the column (see "preview" at the bottom).','blue');
+      };
+    }
+    out("now you may start recording grades (you can change settings at anytime using commands).",'lime')
+  }
+}
+
 
 function out(s,color){
   if (typeof color == 'undefined'){
@@ -50,7 +83,6 @@ function handleFileSelect(evt) {
       output += ' - FileType: ' + (file.type || 'n/a') + '<br />\n';
       output += ' - FileSize: ' + file.size + ' bytes<br />\n';
       output += ' - LastModified: ' + (file.lastModifiedDate ? file.lastModifiedDate.toLocaleDateString() : 'n/a') + '<br />\n';
-  alert(file);
   // read the file contents
   printTable(file);
 
@@ -58,24 +90,16 @@ function handleFileSelect(evt) {
   $('#list').append(output);
 }
 
-function printTable(file) {
-  var reader = new FileReader();
-  reader.readAsText(file);
-  reader.onload = function(event){
-    var csv = event.target.result;
-    var data = $.csv.toArrays(csv);
-    window.data1=data
+function printTable(data) {
     var html = '';
     for(var row in data) {
-      html += '<tr>\r\n';
+      html += '<thead><tr>\r\n';
       for(var item in data[row]) {
         html += '<td>' + data[row][item] + '</td>\r\n';
       }
-      html += '</tr>\r\n';
+      html += '</tr></thead>\r\n';
     }
-    $('#contents').html(html);
-  };
-  reader.onerror = function(){ alert('Unable to read ' + file.fileName); };
+    $('#preview-content').html(html);
 }
 
 function saveCSV(filename, rows) {
@@ -101,4 +125,54 @@ function saveCSV(filename, rows) {
 function beep() {
   var sound = document.getElementById("player");
   sound.play();
+}
+
+function range(start, end, step) {
+    var range = [];
+    var typeofStart = typeof start;
+    var typeofEnd = typeof end;
+
+    if (step === 0) {
+        throw TypeError("Step cannot be zero.");
+    }
+
+    if (typeofStart == "undefined" || typeofEnd == "undefined") {
+        throw TypeError("Must pass start and end arguments.");
+    } else if (typeofStart != typeofEnd) {
+        throw TypeError("Start and end arguments must be of same type.");
+    }
+
+    typeof step == "undefined" && (step = 1);
+
+    if (end < start) {
+        step = -step;
+    }
+
+    if (typeofStart == "number") {
+
+        while (step > 0 ? end >= start : end <= start) {
+            range.push(start);
+            start += step;
+        }
+
+    } else if (typeofStart == "string") {
+
+        if (start.length != 1 || end.length != 1) {
+            throw TypeError("Only strings with one character are supported.");
+        }
+
+        start = start.charCodeAt(0);
+        end = end.charCodeAt(0);
+
+        while (step > 0 ? end >= start : end <= start) {
+            range.push(String.fromCharCode(start));
+            start += step;
+        }
+
+    } else {
+        throw TypeError("Only string and number types are supported");
+    }
+
+    return range;
+
 }
